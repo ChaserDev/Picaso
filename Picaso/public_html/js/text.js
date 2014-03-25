725a96354ce222b55336d78795ab126f147a1d7d
milletf1 <timmiller83@gmail.com>
2014-03-26 09:34:37 +1300

    text appender done

--- Picaso/public_html/js/text.js
+++ Picaso/public_html/js/text.js
@@ -1,37 +1,35 @@
 // TIM OWNER
 
 //converts a string to all lowercase and strips spaces and punctuation
-function convertString() {
+function appendToStringArray(string, tag) {
 
 //stub string/*
-var stubString = "";
+var tempString = "";
 
 
 $.getJSON( "http://baconipsum.com/api/?type=meat-and-filler", function(result){
 	
 		$.each(result, function(key, val) {
-			stubString += (val);
+			tempString += (val);
 		});
 		
 		//convert to lowercase
-		stubString = stubString.toLowerCase();
+		tempString = tempString.toLowerCase();
 
 		//string that holds only the characters from the stubstring
 		var outputString = '';
 
 		for (var i = 0; i < stubString.length; i++)
 		{
-			var upperCaseChar = stubString.charAt(i).toUpperCase();
-			var lowerCaseChar = stubString.charAt(i);
+			var upperCaseChar = tempString.charAt(i).toUpperCase();
+			var lowerCaseChar = tempString.charAt(i);
 		
 			//compare the two chars, if they are different it is a character
 			if(upperCaseChar != lowerCaseChar)	
 			{
-				outputString += lowerCaseChar;
+				string += string.concat(lowerCaseChar);
 			}	
 		}	
-		
-		return outputString;
 	});	
 }
 
