d3e4e37e531ceb605284d4dfbcf1dd17036f5ccf
milletf1 <timmiller83@gmail.com>
2014-03-31 17:40:15 +1300

    removed most console.log()

--- Picaso/public_html/js/color.js
+++ Picaso/public_html/js/color.js
@@ -1,43 +1,54 @@
-/*
-
+/**********************
 		color.js
-*/
+**********************/
+
+//Array indexes for the colorArray.
 var R = "0";	
 var B = "1";
 var G = "2";
 
+//Default characters to find colors for on window load.  used to initialize sessionChars. 
 var DEFAULT_CHARS = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#.,?!";
 
-var ALPHABET_LENGTH = 26;
-var NUMBER_LENGTH = 10;
-var LOWER_CASE_START = 97;
-var UPPER_CASE_START = 65;
-var NUMBER_START = 48;
-var COLOR_ARRAY_LENGTH = 63;
+var DEFAULT_COLOR_TAG = "other";
+var DEFAULT_COLOR = "#000000";
+
+var COLOR_RANGE = 256;
+
 var DEFAULT_IMAGE_TAG_SEARCH = "otago";
 var COLOR_JS_API_PREFIX = "https://api.instagram.com/v1/tags/";
 var COLOR_JS_API_SUFFIX = "/media/recent?callback=?&amp;client_id=4e32d268a27b498e8c9e7840c7863f11";
 
+//myURL, nextURL, and myTag are used to make calls to the instagram API.
 var myURL;
 var nextURL;
 var myTag;
 
+//colorArray holds a dynamic array of colors based on an instagram tag search.
 var colorArray;
 var colorArrayCounter;
+
+//imageArray holds the URLs returned from the instagram tag search.
 var imageArray;
+
+//sessionChars is an associative array. each char in DEFAULT_CHARS is assigned a random color.
+//if a call is made to getColor with a char not in this array, it fetches a new color to associate 
+//with the given char.
 var sessionChars;
+
+//colorArrayReady is a boolean used to determine when the colorArray is fully initialized.
+//It is used to make a decision about whether to rebuild the colorArray, or add new colors to the end of it.
 var colorArrayReady;
 
 /*************************************************************
 	color.js initialization.  Must be called by window.onload
 *************************************************************/
 function colorInit() {
-
-	//myTag = DEFAULT_IMAGE_TAG_SEARCH;
+	
 	sessionChars = DEFAULT_CHARS;
 		
 	colorArray = [];	
-	colorArray["other"] = "#000000";
+	colorArray[DEFAULT_COLOR_TAG] = DEFAULT_COLOR;
 	
 	imageArrayPointer = 0;
 	createStaticArray();
@@ -61,14 +72,14 @@ function createStaticArray() {
 	adds a random color to the color array at the given parameter
 *****************************************************************/
 function addRandomColorToColorArray(charCode) {
-	console.log("addRandomColorToColorArray " + charCode);
+
 	var rgbArray = [];
 	
 	//generate a random hexadecimal color
 	for(var i = 0; i < 3; i++) {
 		
 		//get a random number
-		value = Math.floor(Math.random()*256);
+		value = Math.floor(Math.random()*COLOR_RANGE);
 		
 		rgbArray[i] = value;
 	}
@@ -80,10 +91,8 @@ function addRandomColorToColorArray(charCode) {
 
 /*
 	gets images from instagram
-	NOTE: next url for images  = JSON.pagination.next_url
 */
 function fetchImages() {
-	console.log("fetchImages");
 	
 	$.getJSON(myURL, 		
 		function(result) {		
@@ -122,8 +131,7 @@ function processNextUrl(url) {
 }
 /*******************************************************/
 function fetchImageColors(iteration){
-	console.log("fetchImageColors " + iteration);
-	//console.log("fetching image colors");
+
 	if(colorArrayReady == false)
 	{
 		var imageURL = imageArray[iteration];	
@@ -144,8 +152,6 @@ function fetchImageColors(iteration){
 				
 				//increment colorArrayCounter
 				colorArrayCounter++;
-				console.log("cac: " + colorArrayCounter);
-				console.log(colorArrayCounter + ": " + color);
 				//check if colorArray needs more colors
 				fetchImageColorsTurnaround(iteration);
 			},
@@ -156,13 +162,10 @@ function fetchImageColors(iteration){
 			}			
 		});
 	}
-	else {
-		console.log("double done!");
-	}
 }
 /*******************************************************/
 function fetchImageColorsTurnaround(iteration) {
-	console.log("fetchImageColorsTurnaround " + iteration);
+
 	if(colorArrayCounter < sessionChars.length)
 	{
 		//increment iteration
@@ -184,12 +187,11 @@ function fetchImageColorsTurnaround(iteration) {
 			colorArrayCounter = 0;
 			//set the colorArrayReady flag to true
 			colorArrayReady = true;
-			console.log("done");
 	}
 }
 /*******************************************************/
 function newImageSearch(newTag) {
-	console.log("nis " + newTag);
+
 	//check that the user has entered a new tag
 	if(myTag != newTag) {
 	
@@ -202,21 +204,16 @@ function newImageSearch(newTag) {
 				tag += myTag[c];
 			}		
 		}
-		console.log("new tag: " + tag);	
 		myURL = buildMyURL(tag);		
 		createDynamicArray();
-	}
-	else {
-		console.log("same tag");
-	}
-	
+	}	
 }
 
 /*********************************************************************************************
 	Creates an array of colors which consist of the dominant color of recent instagram images.
 *********************************************************************************************/
 function createDynamicArray() {
-	console.log("createDynamicArray");
+
 	colorArrayReady =false;
 	
 	colorArrayCounter = 0;
@@ -229,7 +226,7 @@ function buildMyURL(tag) {
 /*******************************************************/
 function getColor(c) {
 	//do some stuff here
-	var returnColor = colorArray['other'];
+	var returnColor = colorArray[DEFAULT_COLOR_TAG];
 	
 	if(sessionChars.indexOf(c) > -1)
 	{
@@ -238,19 +235,14 @@ function getColor(c) {
 	else
 	{
 		if(colorArrayReady) {
-			console.log("restarting color array");
 			colorArrayCounter = sessionChars.length;
-			console.log("cac: " + colorArrayCounter);
 			fetchImages();
 		}
 			
 		//Make sure the given char is not undefined.
 		if(c != undefined) {
 			sessionChars += c;
-			console.log("charArray: " + sessionChars);
-			returnColor = colorArray['other'];
 		}	
-	}
-	
+	}	
 	return returnColor;
 }
